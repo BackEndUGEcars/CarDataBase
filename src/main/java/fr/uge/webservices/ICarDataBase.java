948cdc780252a70c08769f06d89876d3c0a15437
LouisBillaut <louisbillaut2000@gmail.com>
2020-11-23 23:56:22 +0100

    bugs fixed and some cars added to the bdd

--- src/main/java/fr/uge/webservices/ICarDataBase.java
+++ src/main/java/fr/uge/webservices/ICarDataBase.java
@@ -8,15 +8,28 @@ import java.rmi.RemoteException;
 import java.util.Map;
 
 public interface ICarDataBase extends Remote {
-    ICar getCar(Long id) throws RemoteException;
-    boolean removeCar(Long id) throws RemoteException ;
-    boolean addCar(ICar t) throws RemoteException ;
-    Map<Long, ICar> getBuyableCar() throws RemoteException ;
-    String toJson() throws RemoteException ;
-    Map<Long, ICar> getAllCars() throws RemoteException ;
-    void init() throws IOException, ParseException, RemoteException ;
-    /*
-    boolean rent(Long id) throws RemoteException ;
-    boolean unrent(Long id) throws RemoteException ;*/
+	ICar getCar(Long id) throws RemoteException;
+
+	boolean removeCar(Long id) throws RemoteException;
+
+	boolean addCar(ICar t) throws RemoteException;
+
+	Map<Long, ICar> getBuyableCar() throws RemoteException;
+
+	String toJson() throws RemoteException;
+
+	Map<Long, ICar> getAllCars() throws RemoteException;
+
+	void init() throws IOException, ParseException, RemoteException;
+
+	boolean rent(Long carId, long employeeId) throws RemoteException;
+
+	long unrent(Long id) throws RemoteException;
+
+	String getBuyableCarsJson() throws RemoteException;
+
+	String getCarJson(long id) throws RemoteException;
+
+	float getPriceOfCar(long id) throws RemoteException;
 
 }
